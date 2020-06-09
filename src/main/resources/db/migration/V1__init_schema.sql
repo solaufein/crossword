611137e7ed69a2efbf785b2f2eb8e6246d9dc0fe
radekw <radoslawwitek@gmail.com>
2020-06-09 21:04:04 +0200

    fix flyway schema

--- src/main/resources/db/migration/V1__init_schema.sql
+++ src/main/resources/db/migration/V1__init_schema.sql
@@ -3,7 +3,7 @@ create schema if not exists crossword;
 create sequence crossword.word_id_seq;
 
 create table crossword.word (
-        id int8 not null default nextval('word_id_seq'),
+        id int8 not null default nextval('crossword.word_id_seq'),
         first_letter varchar(1),
         question varchar(500),
         total_letters int4,
